a589b30e27849f25ad8cc05fdd25e1660db1f60d
Ben Lepsch <blepsch@gmail.com>
2024-04-11 13:00:14 -0400

    add max range to projectile

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -8,7 +8,7 @@ public class ProjectileMovement : MonoBehaviour
     private Transform initial;
 
     private float speed = 25;
-    private float range;
+    private float range = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -33,5 +33,9 @@ public class ProjectileMovement : MonoBehaviour
 
         float dist = Vector3.Distance(transform.position, initial.position);
         Debug.Log("Projectil distance: " + dist);
+
+        if (dist > range) {
+            Destroy(gameObject);
+        }
     }
 }
