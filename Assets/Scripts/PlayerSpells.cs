ef1780048e839302549359968e2fd66ef86ec79e
Ben Lepsch <blepsch@gmail.com>
2024-04-11 12:56:14 -0400

    projectile

--- Assets/Scripts/PlayerSpells.cs
+++ Assets/Scripts/PlayerSpells.cs
@@ -18,22 +18,19 @@ public class PlayerSpells : NetworkBehaviour
         if (Input.GetButtonDown("Fire1")) {
             // Debug.Log("casting spell or soemthing");
 
+            initial = transform;
             Transform pp = Instantiate(projectilePrefab);
             pp.GetComponent<NetworkObject>().Spawn(true);
             pp.parent = transform;
 
-            Debug.Log("Spawnd projectile at " + pp.position);
-            Debug.Log("Parent (player) at " + transform.position);
+            // Debug.Log("Spawnd projectile at " + pp.position);
+            Debug.Log("Parent (player) at " + transform.position + " with rotation " + transform.rotation);
 
             //initial = transform.parent.position;
 
             // ---- player script ----
             // set global variables to save value of inital position + direction (player rotation)
             // spawn projectile as child of player
-            
-            // ---- projectile script ----
-            // (start) find and save initial position + direction values
-            // (update) move projectile, check distance from initial position, despawn if collision or too far
         }
     }
 }
---
+++ Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+
+    private Transform initial;
+
+    private float speed = 25;
+    private float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ---- projectile script ----
+        // (start) find and save initial position + direction values
+        PlayerSpells ps = (PlayerSpells) GameObject.Find(transform.parent.name).GetComponent("PlayerSpells");
+        Debug.Log("Found parent: " + ps);
+        initial = ps.initial;
+        Debug.Log("Initial transform: " + initial.position + ", " + initial.rotation);
+        transform.parent = null;
+
+        transform.position = initial.position;
+        transform.rotation = initial.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // (update) move projectile, check distance from initial position, despawn if collision or too far
+        transform.Translate(transform.forward*Time.deltaTime*speed);
+
+        float dist = Vector3.Distance(transform.position, initial.position);
+        Debug.Log("Projectil distance: " + dist);
+    }
+}
